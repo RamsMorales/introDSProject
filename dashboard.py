293a99ef1a2d8401ab5797aa3d16329c51544b52
Ramson Munoz <ramsonmunoz@Ramsons-Work-Computer.local>
2026-04-06 12:59:14 -0400

    changed data visualization tab to findings

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
-import pandas as pd # Pandas for data manipulation
-import streamlit as st # Streamlit library for web apps (dashboards for our data)
+import pandas as pd  # Pandas for data manipulation
+import streamlit as st  # Streamlit library for web apps (dashboards for our data)
 import plotly.express as px  # Plotly Express for creating charts
 
 st.title("Final Project")  # Title of the dashboard
@@ -13,27 +13,35 @@ st.divider()
 st.sidebar.header("Load Datasets")
 file_uploaded = st.sidebar.file_uploader("Upload a file", type=["csv"])
 if file_uploaded is not None:
-    df = pd.read_csv(file_uploaded) 
+    df = pd.read_csv(file_uploaded)
 else:
     df = pd.read_csv("data/combined_data_hourly.csv")
     # we just want to include the records from 2018 to 2025
     df = df[df["Date"].between("2018-01-01", "2025-12-31")]
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Preliminary Data Analysis","EDA", "Data Visualization", "Hypothesis Testing", "ML Forecast"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(
+    [
+        "Preliminary Data Analysis",
+        "EDA",
+        "Key findings",
+        "Hypothesis Testing",
+        "ML Forecast",
+    ]
+)
 
-with tab1: # Preliminary Data Analysis
-     st.subheader("Preliminary Data Analysis")
-     st.write("First let's take a look at the data")
-     st.write(df.head())
+with tab1:  # Preliminary Data Analysis
+    st.subheader("Preliminary Data Analysis")
+    st.write("First let's take a look at the data")
+    st.write(df.head())
 
-     st.write("Number of rows and columns:")
-     st.write(df.shape)
+    st.write("Number of rows and columns:")
+    st.write(df.shape)
 
-     st.write("Data types:")
-     st.write(df.dtypes)
+    st.write("Data types:")
+    st.write(df.dtypes)
 
-     st.write("Description of each variable:")
-     st.write(df.describe())
+    st.write("Description of each variable:")
+    st.write(df.describe())
 
 # with tab2: # EDA
 #     st.subheader("Charts")
@@ -47,7 +55,7 @@ with tab1: # Preliminary Data Analysis
 #     with st.expander("Line Chart"):
 #         col1, col2 = st.columns([1,4]) # two columns, first one is 1/5 of the width, second one is 4/5 of the width
 #         with col1:
-#             parameter_selected = st.selectbox("Select Parameter", 
+#             parameter_selected = st.selectbox("Select Parameter",
 #                                               df.columns) # user selection
 #             color_selected = st.color_picker("Select a Color",
 #                                               "#6495ED") # user color selection, with defualt #6495ED
@@ -68,15 +76,15 @@ with tab1: # Preliminary Data Analysis
 #                              title="3D Scatter Plot of Water Depth")
 #         fig3.update_scenes(zaxis_autorange='reversed') # Reverse the z-axis to have depth going downwards (because is the water depth)
 #         st.plotly_chart(fig3)
-#     with st.expander("Box Plot"):
-#         fig5 = px.box(df,
-#                       y="Salinity (ppt)",
-#                       title="Box Plot of Salinity")
-#         st.plotly_chart(fig5)
-        
+# with st.expander("Box Plot"):
+#    numeric_columns = df.select_dtypes(include="number").columns
+#    feature_selector = st.selectbox("Select a parameter to visualize", numeric_columns)
+#    fig5 = px.box(df, x=f"{feature_selector}", title=f"Box Plot of {feature_selector}")
+#    st.plotly_chart(fig5)
+
 #     with st.expander("Histogram"):
 #         col1, col2 = st.columns([1,4]) # two columns, first one is 1/5 of the width, second one is 4/5 of the width
-#         with col1: 
+#         with col1:
 #             parameter_selected2 = st.selectbox("Select Parameter 2", df.columns) # user selection
 #         with col2:
 #             fig6 = px.histogram(df,
@@ -107,3 +115,12 @@ with tab1: # Preliminary Data Analysis
 # # TODO: add more charts, such as
 # # box plots, histograms, and heatmaps for correlation (as seen in last week's class)
 
+#  with tab5: # ML forecast
+## Feature engineering
+### TODO introduce lags for random forrest
+
+### TODO Cross validation for param selection in the random forrest setup
+
+### TODO Plot of per fold metrics side by side column chart of validation error per fold vis RMSE, MAPE
+
+### TODO FINAL CHART Test RMSE MAPE
