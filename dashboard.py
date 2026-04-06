afe3462b8f0b0412fcbe10c1b1638bde1521c6f2
valenkloster <valekloster18@gmail.com>
2026-04-06 13:35:18 -0400

    removed the CSV uploeader

--- dashboard.py
+++ dashboard.py
@@ -10,14 +10,10 @@ st.subheader("Forcasting real time energy demand")  # Header of the dashboard
 
 st.divider()
 
-st.sidebar.header("Load Datasets")
-file_uploaded = st.sidebar.file_uploader("Upload a file", type=["csv"])
-if file_uploaded is not None:
-    df = pd.read_csv(file_uploaded)
-else:
-    df = pd.read_csv("data/combined_data_hourly.csv")
-    # we just want to include the records from 2018 to 2025
-    df = df[df["Date"].between("2018-01-01", "2025-12-31")]
+
+df = pd.read_csv("data/combined_data_hourly.csv")
+# we just want to include the records from 2018 to 2025
+df = df[df["Date"].between("2018-01-01", "2025-12-31")]
 
 tab1, tab2, tab3, tab4, tab5 = st.tabs(
     [
